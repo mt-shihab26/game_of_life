60279520ea873b9d03b5ac36a7b35210d6b7b507
Shihab Mahamud <shihab4t@gmail.com>
2026-04-14 04:57:06 +0600

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    event::{self, Event},
+    style::{self, Stylize},
+    terminal,
+};
+use std::{
+    io::{self, Write},
+    time::Duration,
+};
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    loop {
+        stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+        if event::poll(Duration::from_millis(500))? {
+            match event::read()? {
+                Event::Key(event) => match event.code {
+                    event::KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    _ => (),
+                },
+                Event::Resize(width, height) => println!("New size {}x{}", width, height),
+                _ => (),
+            }
+        }
+
+        stdout
+            .queue(cursor::MoveTo(0, 0))?
+            .queue(style::PrintStyledContent("Hello World".magenta()))?;
+
+        stdout.flush()?;
+    }
 }
